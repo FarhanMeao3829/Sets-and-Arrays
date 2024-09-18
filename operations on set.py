0acc855f0d87d7b775adb7296b754b67cd84f44e
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-09-18 09:53:51 +0600

    first commit

---
+++ operations on set.py
@@ -0,0 +1,17 @@
+mySet = {1, 2, 3}
+print(mySet)
+
+mySet2 = {2.0, "Nahian sir is bad", (1, 2, 3)}
+print(mySet2)
+
+li = ["Farhan", "Aditya", "Nahian"]
+
+setList = set(li)
+print(setList)
+
+setList.add("Gigachad")
+print(setList)
+
+setList.pop()
+print(setList)
+
